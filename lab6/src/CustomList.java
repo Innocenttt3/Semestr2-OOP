656e0798cc21baee44e6a4795e2ab4107be406d6
Innocenttt3 <122268653+Innocenttt3@users.noreply.github.com>
2023-06-08 15:25:53 +0200

    lab6 halfdone

--- lab6/src/CustomList.java
+++ lab6/src/CustomList.java
@@ -1,12 +1,126 @@
-public class CustomList<T> {
+import java.util.AbstractList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CustomList<T> extends AbstractList<T> {
     public class Node {
+        T data;
+        Node nextOne;
+
+        Node(T value) {
+            this.data = value;
+            this.nextOne = null;
+        }
+    }
+
+    private Node tail;
+    private Node head;
+
+    public CustomList() {
+        tail = null;
+        head = null;
+    }
+
+    @Override
+    public int size() {
+        int counter = 0;
+        Node tempNode = head;
+        while (tempNode != null) {
+            tempNode = tempNode.nextOne;
+            counter++;
+        }
+        return counter;
+    }
+
+    @Override
+    public T get(int index) {
+        int tmpIndex = 0;
+        if (index > size()) {
+            throw new RuntimeException("index is out of size");
+        }
+        Node indexFinder = head;
+        while (tmpIndex < index) {
+            indexFinder = indexFinder.nextOne;
+            tmpIndex++;
+        }
+        return indexFinder.data;
+    }
+
+    public void addLast(T newValue) {
+        Node newNode = new Node(newValue);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.nextOne = newNode;
+            tail = newNode;
+        }
+    }
+
+    public T getLast() {
+        return tail.data;
+    }
+
+    public void addFirst(T newValue) {
+        Node newNode = new Node(newValue);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.nextOne = head;
+            head = newNode;
+        }
+    }
+
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node tmpNode = head;
+            @Override
+            public boolean hasNext() {
+                if(tmpNode == null) {return false;}
+                return tmpNode.nextOne != null;
+            }
+            @Override
+            public T next() {
+                T previous = head.data;
+                tmpNode = head.nextOne;
+                return null;
+            }
+        };
+    }
+
+    public T getFirst() {
+        return head.data;
+    }
+
+    public T removeFirst() {
+        Node oldRoot = this.head;
+        this.head = oldRoot.nextOne;
+        return oldRoot.data;
+    }
+
+    public T removeLast() {
+        if (head == null) {
+            throw new NoSuchElementException("Lista jest pusta.");
+        }
         T value;
-        Node next;
-        public Node(T value, Node next) {
-            this.value = value;
-            this.next = next;
-        }§
-        private Node head;
-        private Node tail;
+        if (head == tail) {
+            value = head.data;
+            head = null;
+            tail = null;
+        } else {
+            Node prev = null;
+            Node current = head;
+            while (current.nextOne != null) {
+                prev = current;
+                current = current.nextOne;
+            }
+
+            value = current.data;
+            prev.nextOne = null;
+            tail = prev;
+        }
+        return value;
     }
 }
+
--- lab6/src/Main.java
+++ lab6/src/Main.java
@@ -3,7 +3,10 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-
-
+        CustomList<Integer> myList = new CustomList<>();
+        myList.addFirst(1);
+        myList.addFirst(2);
+        myList.addFirst(3);
+        System.out.println(myList.get(0));
     }
 }
